b30d3432ab27154880b47ffc34b69cfa9bbbb128
Shiv Bhonde <shivbhonde04@gmail.com>
2022-10-22 00:26:02 +0530

    setup basic repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Home, Profile } from "./pages";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<h1 className="text-red-500 text-2xl text-center border-2 mt-4">
+				Git For Geeks
+			</h1>
+			<Home />
+			<Profile />
+		</>
+	);
 }
 
 export default App;
