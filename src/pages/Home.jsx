4aea7dc768823b05a7d667ebf6d64d0eae256b65
MurtazaShikari <114511800+MurtazaShikari@users.noreply.github.com>
2022-10-23 11:13:09 +0530

    Updated HomePage and configured tailwind team

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<>
+			{/* <div className="z-0  w-full h-screen bg-[url('')] bg-auto"> */}
+				<div className="box-border absolute top-48 border-solid border-2 border-black left-20 md:left-50 rounded-t-2xl w-910 bg-white">
+					<div className="con bg-brightOlive rounded-t-2xl h-10">
+						<div className="p-1 text-2xl text-right">BROUGHT TO YOU BY<img src="" alt="" /></div>
+					</div>
+						<div className="max-w-80 p-10 text-4xl text-center">Git For Geeks</div>
+						<div className="grid lg:grid-cols-2 sm:grid-cols-1 p-5 gap-5 text-center text-bold">
+							<div className="name bg-brightOlive border-solid border-2 border-black rounded-full  p-2">Murtaza Shikari</div>
+							<div className="name bg-brightOlive border-solid border-2 border-black rounded-full  p-2">Mahek Jain</div>
+							<div className="name bg-brightOlive border-solid border-2 border-black rounded-full  p-2">Priyanka Ramachandran</div>
+							<div className="name bg-brightOlive border-solid border-2 border-black rounded-full  p-2">Name 4</div>
+							<div className="name bg-brightOlive border-solid border-2 border-black rounded-full  p-2">Name 5</div>
+							<div className="name bg-brightOlive border-solid border-2 border-black rounded-full  p-2">Name 6</div>
+							<div className="name bg-brightOlive border-solid border-2 border-black rounded-full  p-2">Name 7</div>
+							<div className="name bg-brightOlive border-solid border-2 border-black rounded-full  p-2">Name 8</div>
+							<div className="name bg-brightOlive border-solid border-2 border-black rounded-full  p-2">Name 9</div>
+						
+					</div>
+				</div>
+			{/* </div> */}
+
+			<img src="" alt="" />
+		</>
+	)
 };
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		maxWidth:{
+			'1/2' : '50%'
+		},
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px'
+		},
+		extend: {
+			colors: {
+				cream: '#FEF5ED',
+				lilOlive: '#D3E4CD',
+				brightOlive: '#ADC2A9',
+				darkOlive: '#99A799'
+			}
+		},
 	},
 	plugins: [],
+	
 };
