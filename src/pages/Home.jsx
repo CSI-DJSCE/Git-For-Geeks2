95f59da3d4557d0d9060f79e9936df549234ded8
unknown <mahekmj123@gmail.com>
2022-10-25 00:15:45 +0530

    added the scrollbar in home page and updated the profile page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,17 +3,17 @@ import React from "react";
 const Home = () => {
   return (
     <>
-      <div className="w-full flex justify-center bg-[url('../public/assets/images/home/homeback.png')] sm:bg-cover sm:bg-no-repeat bg-center">
+      <div className="w-full flex justify-center bg-[url('../public/assets/images/home/homeback.png')] sm:bg-cover sm:bg-no-repeat bg-center scrollbar-hide">
         <div className="relative box-border mx-auto my-10 border-solid border-2 border-black rounded-t-2xl">
           {/* Floating images start */}
           <img
             src="/assets/images/home/element.png"
-            className="-z-1 block absolute object-contain z-2 top-[50%] -left-[35%]"
+            className="block absolute object-contain top-[50%] -left-[35%] z-[2]"
             alt=""
           />
           <img
             src="/assets/images/home/2.png"
-            className=" -z-1 block absolute object-contain z-2 top-[10%] -right-[30%]"
+            className="block absolute object-contain top-[10%] -right-[30%] z-[2]"
             alt=""
           />
           {/* Floating images end */}
@@ -37,65 +37,67 @@ const Home = () => {
           {/* Header ens */}
 
           {/* GFG Body start */}
-          <div className="bg-white px-36">
-            <div
-              className="flex flex-row max-w-80 p-10 
+          <div className="h-screen scrollbar scrollbar-thumb-[#D9D9D9] overflow-y-scroll scrollbar-thumb-rounded-lg scrollbar-track-rounded-full">
+            <div className="bg-white px-36">
+              <div
+                className="flex flex-row max-w-80 p-10 
 										text-9xl text-center font-gothic font-normal tracking-wider ml-20"
-            >
-              <p>GIT</p>
-              <img src="/assets/images/home/git.png" alt="" />
-            </div>
-            <div className="text-7xl text-center font-gothic font-normal tracking-wider mb-20 ">
-              FOR GEEKS!
-            </div>
+              >
+                <p>GIT</p>
+                <img src="/assets/images/home/git.png" alt="" />
+              </div>
+              <div className="text-7xl text-center font-gothic font-normal tracking-wider mb-20 ">
+                FOR GEEKS!
+              </div>
 
-            {/* <div className="flex flex-row justify-center items-center max-w-md font-bold text-center mx-20 bg-darkOlive  border-solid border-2 border-black rounded-full mb-[3rem]">
+              {/* <div className="flex flex-row justify-center items-center max-w-md font-bold text-center mx-20 bg-darkOlive  border-solid border-2 border-black rounded-full mb-[3rem]">
 						<div className="pb-5 mr-20 mt-5 font-gothic font-medium uppercase text-center text-xl">Search Your Name Here
 						</div>
 						<div>
 						<img src="/assets/images/home/searchbar.png" className="ml-40"alt="" />
 						</div>
 						</div> */}
-            <div className="flex justify-center items-center">
-              <div className="w-[30rem] border-b-8 p-2 flex justify-between space-x-1 text-black font-bold text-center mx-20 bg-darkOlive  border-solid border-2 border-black rounded-3xl mb-[3rem]">
-                <input
-                  type="text"
-                  className="border-none font-bold text-center text-black bg-darkOlive py-2 placeholder-black focus:outline-none "
-                  placeholder="SEARCH YOUR NAME HERE"
-                />
-                <button className="px-4 text-white bg-black rounded-full ">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-5 h-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                    />
-                  </svg>
-                </button>
+              <div className="flex justify-center items-center">
+                <div className="w-[30rem] border-b-8 p-2 flex justify-between space-x-1 text-black font-bold text-center mx-20 bg-darkOlive  border-solid border-2 border-black rounded-3xl mb-[3rem]">
+                  <input
+                    type="text"
+                    className="border-none font-bold text-center text-black bg-darkOlive py-2 placeholder-black focus:outline-none "
+                    placeholder="SEARCH YOUR NAME HERE"
+                  />
+                  <button className="px-4 text-white bg-black rounded-full ">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-5 h-5"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+
+              <div className="grid md:grid-cols-2 sm:grid-cols-1 gap-8 py-5 text-center text-bold">
+                <div className="name-pills primary-btn-click">Murtaza Shikari</div>
+                <div className="name-pills primary-btn-click">Mahek Jain</div>
+                <div className="name-pills primary-btn-click">Priyanka Ramachandran</div>
+                <div className="name-pills primary-btn-click">Name 4</div>
+                <div className="name-pills primary-btn-click">Name 5</div>
+                <div className="name-pills primary-btn-click ">Name 6</div>
+                <div className="name-pills primary-btn-click">Name 7</div>
+                <div className="name-pills primary-btn-click">Name 8</div>
+                <div className="name-pills primary-btn-click">Name 9</div>
+                <div className="name-pills primary-btn-click">Name 10</div>
               </div>
             </div>
-
-            <div className="grid md:grid-cols-2 sm:grid-cols-1 gap-8 py-5 text-center text-bold">
-              <div className="name-pills primary-btn-click">Murtaza Shikari</div>
-              <div className="name-pills primary-btn-click">Mahek Jain</div>
-              <div className="name-pills primary-btn-click">Priyanka Ramachandran</div>
-              <div className="name-pills primary-btn-click">Name 4</div>
-              <div className="name-pills primary-btn-click">Name 5</div>
-              <div className="name-pills primary-btn-click ">Name 6</div>
-              <div className="name-pills primary-btn-click">Name 7</div>
-              <div className="name-pills primary-btn-click">Name 8</div>
-              <div className="name-pills primary-btn-click">Name 9</div>
-              <div className="name-pills primary-btn-click">Name 10</div>
-            </div>
+            {/* GFG Body end */}
           </div>
-          {/* GFG Body end */}
         </div>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
-	content: ["./src/**/*.{js,jsx,ts,tsx}"],
-	theme: {
-		extend: {
-			maxWidth: {
-				"1/2": "50%",
-			},
-			screens: {
-				sm: "480px",
-				md: "768px",
-				lg: "976px",
-			},
-			fontFamily: {
-				gothic: ["Franklin Gothic Medium", "Georgia", "sans-serif"],
-			},
-			fontSize: {
-				xl: [
-					"24px",
-					{
-						lineHeight: "27.21px",
-					},
-				],
-			},
-			borderWidth: {
-				3: "3px",
-			},
-			colors: {
-				darkOlive: "#ACC2A8",
-				white: "#FFFFFF",
-				cream: "#FEF5ED",
-				lilOlive: "#D3E4CD",
-				brightOlive: "#ADC2A9",
-				icon1: "#D1D184",
-				icon2: "#525C52",
-				icon3: "#DBFBB1",
-			},
-		},
-	},
-	plugins: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      maxWidth: {
+        "1/2": "50%",
+      },
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+      },
+      fontFamily: {
+        gothic: ["Franklin Gothic Medium", "Georgia", "sans-serif"],
+      },
+      fontSize: {
+        xl: [
+          "24px",
+          {
+            lineHeight: "27.21px",
+          },
+        ],
+      },
+      borderWidth: {
+        3: "3px",
+      },
+      colors: {
+        darkOlive: "#ACC2A8",
+        white: "#FFFFFF",
+        cream: "#FEF5ED",
+        lilOlive: "#D3E4CD",
+        brightOlive: "#ADC2A9",
+        icon1: "#D1D184",
+        icon2: "#525C52",
+        icon3: "#DBFBB1",
+      },
+    },
+  },
+  // eslint-disable-next-line no-undef
+  plugins: [require("tailwind-scrollbar"), require("tailwind-scrollbar-hide")],
+  variants: {
+    scrollbar: ["rounded"],
+  },
 };
