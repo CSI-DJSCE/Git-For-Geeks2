4b08f4f2564c6e7ee47356164808c077b43587b0
MurtazaShikari <114511800+MurtazaShikari@users.noreply.github.com>
2022-10-30 11:31:14 +0530

    Updated Images and position

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,28 +23,29 @@ const Home = () => {
   return (
     <div className="w-full flex flex-col items-center space-y-6 bg-[url('../public/assets/images/home/homeback.png')] sm:bg-cover sm:bg-no-repeat bg-center relative overflow-x-hidden px-4 md:px-20 h-screen">
       {/* Floating images start */}
-      {/* <img
-        src="/assets/images/home/element.png"
-        className="hidden w-[20rem] md:block absolute object-contain z-[10000] md:top-[0] md:left-[22%] "
-      />
       <img
-        src="/assets/images/home/2.png"
-        className="hidden z-[10000]  absolute object-contain md:block md:top-[0] md:right-[30%]"
-        alt=""
+        src="/assets/images/home/element.png"
+        className="hidden w-[20rem] md:block absolute object-contain z-[10000]  md:w-[14rem] md:top-[60%] md:left-[0%] "
       />
+
       <img
         src="/assets/images/home/STARRR.png"
-        className="hidden  absolute object-contain md:block z-[10000]  md:top-[0] md:right-[15%]"
+        className="hidden  absolute object-contain md:block z-[10000]  md:w-[12rem] md:top-[60%] md:right-[3%]"
+        alt=""
+      />
+      <img
+        src="/assets/images/home/Date.png"
+        className="hidden z-[10000]  absolute object-contain md:block md:w-[14rem] md:top-[15%] md:left-[1%]"
         alt=""
       />
 
       <img
-        src="/assets/images/home/seminarhall.png"
-        className="hidden  absolute object-contain md:block z-[10000]  md:top-[15%] md:-right-[16%]"
+        src="/assets/images/home/Location.png"
+        className="hidden  absolute object-contain md:block z-[10000] md:w-[17rem] md:top-[15%] md:right-[0%]"
         alt=""
       />
 
-      <img
+      {/*<img
         src="/assets/images/home/image-removebg-preview.png"
         className="hidden w-[20rem]  absolute object-contain  md:block md:top-[11%] md:-left-[22%] "
         alt=""
